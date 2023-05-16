d4a1536fd7824a6b3306821de59a1950d306102e
shubham singla <shubham.singla@sparkbrains.ingit config --global user.name shubham>
2023-05-16 14:52:45 +0530

    changes till prices

--- components/PizzaCard.jsx
+++ components/PizzaCard.jsx
@@ -1,10 +1,13 @@
 import Image from "next/image";
 import styles from "../styles/PizzaCard.module.css";
+import Link from "next/link";
 
 const PizzaCard = ({pizza}) => {
   return (
     <div className={styles.container}>
-      <Image src="/img/pizza.png" alt="" width="500" height="500" />
+      <Link href={`/product/${pizza._id}`} passHref>
+        <Image src="/img/pizza.png" alt="" width="500" height="500" />
+      </Link>
       <h1 className={styles.title}>{pizza?.title}</h1>
       <span className={styles.price}>${pizza?.prices[0]}</span>
       <p className={styles.desc}>
